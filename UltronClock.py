d16d16925ec8bcfc22e4d15f5efa5a18d519c6c4
ain-udayathilagan-elamaran <udayathilagan.elamaran@aparinnosys.com>
2021-11-18 14:56:22 +0530

    Revert "added error log"
    This reverts commit a7258fe390b1edf0ac6dcd8ac9fa364480bf7aa4.

--- UltronClock.py
+++ UltronClock.py
@@ -235,18 +235,8 @@ def Set_Cam_Time(cameraIP,username,password,year,month,day,hour,min,sec):
     
     Cam_State,Cam_uuid=CAM.Get_UID(cameraIP=cameraIP,username=username,password=password)
     if Cam_State:
-        Mstate,mresponse=CAM.Set_Mannual_Time(cameraIP=cameraIP,year=year,month=month,day=day,hour=hour,min=min,sec=sec,uID=Cam_uuid)
-        if Mstate:
-            logger.info("Set manual time for camera {}".format(str(cameraIP)))
-        else:
-            logger.error("Error ON Set manual time for camera {}".format(str(cameraIP)))
-            logger.error(str(mresponse))
-        Nstate,Nresponse=CAM.Set_NTP_Time(cameraIP,uID=Cam_uuid)
-        if Nstate:
-            logger.info("Set NTP time for camera {}".format(str(cameraIP)))
-        else:
-            logger.error("Error ON Set NTP time for camera {}".format(str(cameraIP)))
-            logger.error(str(Nresponse))
+        CAM.Set_Mannual_Time(cameraIP=cameraIP,year=year,month=month,day=day,hour=hour,min=min,sec=sec,uID=Cam_uuid)
+        CAM.Set_NTP_Time(cameraIP,uID=Cam_uuid)
     else :
         logger.info("Can't Access Cam : "+str(cameraIP))
 
